0fb5a520e1801cf58826edda9331f774d9e23823
Chengyu Peng <chengyupeng0125@gmail.com>
2023-02-22 22:23:30 +0800

    react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Layout from './pages/layout';
+import Login from './pages/login';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Layout />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
